836a388b61f0139c5f302d0b23f09649514d1b1f
Talita Leal <86267204+lealtalita@users.noreply.github.com>
2022-07-04 21:36:31 -0300

    Add files via upload

---
+++ ComecaB.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] inventory = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string items in inventory)
+{
+    if (items.StartsWith("B"))
+        Console.WriteLine($"{items} starts with 'B'!");
+}
+
